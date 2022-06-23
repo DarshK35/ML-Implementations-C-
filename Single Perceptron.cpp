763d17bbf9b06b00747779dc90b6c516287900bf
Darsh Kumar <darsh.kumar2002@gmail.com>
2022-06-23 16:46:11 +0530

    Changed matrix definitions to float

--- Single Perceptron.cpp
+++ Single Perceptron.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 
 // Useful Exceptions
+struct MatrixInverseException : exception {
+	const char *what() const noexcept {
+		return "Matrix inapplicable for operation";
+	}
+};
 struct MatrixOperationException : exception {
 	const char* what() const noexcept {
 		return "Invalid Matrix sizes";
@@ -23,10 +28,9 @@ struct MatrixAccessException : exception {
 	is stored as [5 7 9 2 4 5]
 */
 
-template <typename t>
 class Matrix {
 	private:
-	t *data;
+	float *data;
 	int rows, cols;
 
 	public:
@@ -46,51 +50,74 @@ class Matrix {
 	}
 
 	// Basic operations
-	void operator=(Matrix<t> mat) {
+	void operator=(Matrix mat) {
 		data = mat.data;
 		rows = mat.rows;
 		cols = mat.cols;
 	}
-	Matrix<t> operator+(Matrix<t> mat) {
+	Matrix operator+(Matrix mat) {
 		if(rows != mat.rows || cols != mat.cols) {
 			throw MatrixOperationException;
 		}
-		Matrix<t> ret = mat;
+		Matrix ret = mat;
 		for(int i = 0; i < rows * cols; i++) {
 			ret.data[i] = data[i] + mat.data[i];
 		}
 		return ret;
 	}
-	Matrix<t> operator-(Matrix<t> mat) {
+	Matrix operator-(Matrix mat) {
 		if(rows != mat.rows || cols != mat.cols) {
 			throw MatrixOperationException;
 		}
-		Matrix<t> ret = mat;
+		Matrix ret = mat;
 		for(int i = 0; i < rows * cols; i++) {
 			ret.data[i] = data[i] - mat.data[i];
 		}
 		return ret;
 	}
-	// Incomplete Function
-	Matrix<t> operator*(Matrix<t> mat) {
+	Matrix operator*(Matrix mat) {
 		if(cols != mat.rows) {
 			throw MatrixOperationException;
 		}
-		Matrix<t> ret = new Matrix<t>(rows, mat.cols);
+		int temp;
+		Matrix ret = new Matrix(rows, mat.cols);
+
+		for(int i = 0; i < ret.rows; i++) {
+			for(int j = 0; j < ret.cols; j++) {
+				temp = 0;
+				for(int k = 0; k < cols; k++) {
+					temp += get(i, k) * mat.get(k, j);
+				}
+
+				ret.set(i, j) = temp;
+			}
+		}
 
 		return ret;
 	}
 
+	// Other Operations
+	Matrix transpose() { 
+		Matrix ret = new Matrix(cols, rows);
+
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				ret.set(j, i) = get(i, j);
+			}
+		}
+
+		return ret;
+	}
 
 	// Location Access operation
-	t get(int r, int c) {
+	float get(int r, int c) {
 		if(r >= rows || r < 0 || c >= cols || c < 0) {
 			throw MatrixAccessException;
 		}
 		int i = r * cols + c;
 		return data[i];
 	}
-	void set(t value, int r, int c) {
+	void set(float value, int r, int c) {
 		if(r >= rows || r < 0 || c >= cols || c < 0) {
 			throw MatrixAccessException;
 		}
